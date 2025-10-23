2b922f0c81185bb50290908fd71d0e71122021df
Armand <52544834+armand124@users.noreply.github.com>
2025-10-23 19:09:12 +0300

    Solved CORS problem

--- backend/api/core/main.py
+++ backend/api/core/main.py
@@ -3,6 +3,7 @@ from app.core.db import Database
 from contextlib import asynccontextmanager
 from app.endpoints import auth
 from app.endpoints import log
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app : FastAPI):
@@ -12,10 +13,24 @@ async def lifespan(app : FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:3000",  # React default
+    "http://localhost:5173",  # Vite default
+    "http://127.0.0.1:5173"
+]
+
 @app.middleware("http")
 async def function_func(request : Request , callable):
     result = await callable(request)
     return result
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,           # or ["*"] to allow all
+    allow_credentials=True,
+    allow_methods=["*"],             # allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],             # allow all headers
+)
 
 #Endpoints
 app.include_router(auth.router)

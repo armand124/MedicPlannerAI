34a5f42a8108fa4febe923cee955eadce89e826f
Dărăbăneanu Liviu-Eugen <darabaneanuliviu@gmail.com>
2025-10-23 19:03:17 +0300

    middleware

--- backend/api/core/app/endpoints/log.py
+++ backend/api/core/app/endpoints/log.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter,Depends
 from app.modules.auth.service import AuthService
 from app.modules.log.service import LogService
 from app.modules.log.model import LogEntry
+from fastapi import FastAPI , Request
 
 router = APIRouter()
 
@@ -9,7 +10,8 @@ router = APIRouter()
     "/logs/"
 )
 async def logs(current_user : dict = Depends(AuthService.get_current_user)):
-    return await LogService.fetch_logs(current_user)
+    result = await LogService.fetch_logs(current_user) /
+    return result
 
 @router.post (
     "/logs/register"
--- backend/api/core/main.py
+++ backend/api/core/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request
 from app.core.db import Database
 from contextlib import asynccontextmanager
 from app.endpoints import auth
@@ -11,6 +11,11 @@ async def lifespan(app : FastAPI):
     await Database.disconnectFromDatabase()
 
 app = FastAPI(lifespan=lifespan)
+
+@app.middleware("http")
+async def function_func(request : Request , callable):
+    result = await callable(request)
+    return result
 
 #Endpoints
 app.include_router(auth.router)

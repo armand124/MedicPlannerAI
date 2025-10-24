b94183d0d2820253b4a13ba0fda57d7d8dc8249c
Armand <52544834+armand124@users.noreply.github.com>
2025-10-24 09:19:56 +0300

    Update main.py

--- backend/api/core/main.py
+++ backend/api/core/main.py
@@ -16,6 +16,14 @@ async def lifespan(app : FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",
+    "http://localhost:8080",
+    "http://127.0.0.1:8080"
+]
+
 @app.middleware("http")
 async def function_func(request : Request , callable):
 
@@ -58,7 +66,7 @@ async def function_func(request : Request , callable):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],          
+    allow_origins=origins,          
     allow_credentials=True,
     allow_methods=["*"],       
     allow_headers=["*"],             
